77f9a21c08af8a567890cfcf5d45925c123803c4
Growithis <lafroujianas@gmail.com>
2022-11-17 13:10:55 +0100

    Making the application work, handle a json post call, calculate the price correctly with the sample data and show the right response

--- routes/api_calls.js
+++ routes/api_calls.js
@@ -4,14 +4,33 @@ This file contains the api routes
 
 */
 
+const API = require("../lib/api/api");
+
+require("../lib/api/api")
+
+
 module.exports = function(app) {
 
     app.post('/checkout', function (req, res) {
 
      
-         data = (req.body)
+        data = (req.body)
      
-         console.log(data)
+        console.log(data)
+
+        api = new API()
+
+        restructured_data= api.restructure_data(data)
+
+        data_grouped_by_number=api.groupByNumber(restructured_data)
+
+        sum = api.sum(data_grouped_by_number)
+
+        console.log("Sum",sum)
+
+        res.end(JSON.stringify({"price":sum}))
+
+         
 
      
      });  
