09662f537cda57e8f140c205f506b590656fc611
Growithis <lafroujianas@gmail.com>
2022-11-24 14:47:35 +0100

    Changing intend space to 2

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ require("./routes/api_calls.js")(app)
 
 
 var server = app.listen(8080, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Listening at http://%s:%s", host, port)
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Listening at http://%s:%s", host, port)
 })
 
 
--- routes/api_calls.js
+++ routes/api_calls.js
@@ -9,29 +9,29 @@ const API = require("../lib/api/api");
 require("../lib/api/api")
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.post('/checkout', function (req, res) {
-
-        
-        data = (req.body)
-     
-        api = new API()
-
-        restructured_data= api.restructure_data(data)
-
-        data_grouped_by_number=api.group_by_number(restructured_data)
-
-        sum = api.sum(data_grouped_by_number)
+  app.post('/checkout', function (req, res) {
 
 
-        res.setHeader('Content-Type', 'application/json');
+    data = (req.body)
+
+    api = new API()
+
+    restructured_data = api.restructure_data(data)
+
+    data_grouped_by_number = api.group_by_number(restructured_data)
+
+    sum = api.sum(data_grouped_by_number)
 
 
-        res.end(JSON.stringify({"price":sum}))
+    res.setHeader('Content-Type', 'application/json');
 
-         
 
-     
-     });  
-  };
+    res.end(JSON.stringify({ "price": sum }))
+
+
+
+
+  });
+};
